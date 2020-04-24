67b55bdb4795add19609e6b368ecf74010510858
Adekoreday <khord4eng@gmail.com>
2020-04-24 09:07:27 +0100

    add AuthServer

---
+++ OAuth.AuthServer/Config.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace OAuth.AuthServer{
+
+    public static class Config{
+
+        public static IEnumerable<ApiResource> ApiResources(){
+            return new[] {
+                new ApiResource ("oauthApi", "OAuth Api")
+            };
+        }
+        public static IEnumerable<Client> Clients(){
+            return new[] {
+                new Client {
+                    ClientId = "oauthApi",
+                    ClientSecrets = new [] { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials
+                }
+            };
+        }
+        public static IEnumerable<TestUser> TestUsers()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "adeyemi@mailinator.com",
+                    Password = "Password"
+                }
+            };
+        }
+
+
+    }
+
+}
