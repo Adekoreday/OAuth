0a7367f65902590e07e64f17be3c67ac7a90c2bf
Adekoreday <khord4eng@gmail.com>
2020-04-26 11:11:43 +0100

    implicit auth flow

--- OAuth.AuthServer/Config.cs
+++ OAuth.AuthServer/Config.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
+using Microsoft.Extensions.Configuration;
 
 namespace OAuth.AuthServer{
 
@@ -12,13 +15,43 @@ namespace OAuth.AuthServer{
                 new ApiResource ("oauthApi", "OAuth Api")
             };
         }
-        public static IEnumerable<Client> Clients(){
+
+       public static IEnumerable<IdentityResource> IdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<Client> Clients(IConfiguration _configuration){
+
+
+            var oauthWebRedirectUrl = _configuration.GetSection("Urls:OAuthWeb:SiginIn").Value;
+            var oauthWebLogoutRedirectUrl = _configuration.GetSection("Urls:OAuthWeb:SiginOut").Value;
+
+            Debug.Assert(!String.IsNullOrEmpty(oauthWebRedirectUrl), "redirect url cannot be empty");
+            Debug.Assert(!String.IsNullOrEmpty(oauthWebLogoutRedirectUrl), "logout redirect url cannot be empty");
             return new[] {
                 new Client {
                     ClientId = "oauthApi",
                     ClientSecrets = new [] { new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
                     AllowedScopes = { "oauthApi" }
+                },
+                new Client {
+                    ClientId = "oauthWeb",
+                    ClientSecrets = new [] { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    RedirectUris = new [] {oauthWebRedirectUrl},
+                    PostLogoutRedirectUris = new [] {oauthWebLogoutRedirectUrl},
+                    AllowedScopes = { 
+                          IdentityServerConstants.StandardScopes.OpenId, //OpenIdConnect scopes
+                          IdentityServerConstants.StandardScopes.Profile,
+                         "oauthApi" //resource scopes
+                        },
+                    AllowOfflineAccess = true
                 }
             };
         }
@@ -30,6 +63,12 @@ namespace OAuth.AuthServer{
                 {
                     SubjectId = "1",
                     Username = "adeyemi@mailinator.com",
+                    Password = "Password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "adeyemi",
                     Password = "Password"
                 }
             };
